d7e6ce251183d498159048103eb5ee96a0f6ebbe
jimhoc94 <73989467+jimhoc94@users.noreply.github.com>
2024-01-13 17:16:43 +0100

    Add main action structure

---
+++ index.js
@@ -0,0 +1,19 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+const main = async () => {
+  try {
+    /**
+     * We need to fetch all the inputs that were provided to our action
+     * and store them in variables for us to use.
+     **/
+    const owner = core.getInput("owner", { required: true });
+
+    console.log("Hello " + owner);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+};
+
+// Call the main function to run the action
+main();
